a8e887f8a611e9bfb38b9657fbfd3db7a7e4dfd9
ylcnky <mehmet.yalcinkaya@aalto.fi>
2017-06-05 22:18:53 +0300

    contollers added

--- models/Comment.js
+++ models/Comment.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose')
 
 var CommentSchema = new mongoose.Schema({
+  username: {type:String, default:''}
   body: {type:String, default:''},
-  
   timestamp: {type: Date, default:Date.now}
 })
 
