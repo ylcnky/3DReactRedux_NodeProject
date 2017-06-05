da06b3a3a69bfc601bb087327a27605a8a8374c5
ylcnky <mehmet.yalcinkaya@aalto.fi>
2017-06-05 22:12:37 +0300

    comment and zone schemas added

--- models/Zone.js
+++ models/Zone.js
@@ -1,1 +1,9 @@
 var mongoose = require('mongoose')
+
+var ZoneSchema = new mongoose.Schema({
+  name: {type:String, default:''},
+  zipCodes:{type:String, default:[]},
+  timestamp: {type: Date, default:Date.now}
+})
+
+module.exports = mongoose.model('ZoneSchema', ZoneSchema)
